0171c08a7c2f1c00ced5555dfe499a4714a23704
jzjizhe <jzjizhe@users.noreply.github.com>
2025-08-25 09:49:21 +0000

    deploy: jzjizhe/my_blogs@9ceeedb70a766d8f4440ea8245fa6c9a8517077f

---
+++ imgs/bg/run.sh
@@ -0,0 +1,8 @@
+python generate_jianbian.py \
+    --direction diagonal \
+# python generate_jianbian.py \
+# --width 2560 --height 1440 \
+# --start "#e3f0ff" \
+# --end "#ede3ff" \
+# --direction horizontal \
+# --output bg.jpg
